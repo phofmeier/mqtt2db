211ed6de3d8c6440c93dbdf7e4aac28a177dd328
phofmeier <phofmeier@googlemail.com>
2024-07-10 21:58:27 +0000

    fix unittests

--- src/dashboard/dash_app.py
+++ src/dashboard/dash_app.py
@@ -4,42 +4,40 @@ from pymongo import MongoClient
 CONNECTION_STRING = "localhost:27017"
 
 
-app = Dash()
+def main():
+    app = Dash()
+    app.layout = [
+        html.H1(children="Mongo DB Dash", style={"textAlign": "center"}),
+    ]
 
+    client = MongoClient(CONNECTION_STRING)
+    db_names = client.list_database_names()
+    app.layout.append(
+        dcc.Dropdown(db_names, db_names[0], id="db-names-selector-dropdown"),
+    )
 
-app.layout = [
-    html.H1(children="Mongo DB Dash", style={"textAlign": "center"}),
-]
+    app.layout.append(html.Div(children="", id="db-table"))
 
-client = MongoClient(CONNECTION_STRING)
-db_names = client.list_database_names()
-app.layout.append(
-    dcc.Dropdown(db_names, db_names[0], id="db-names-selector-dropdown"),
-)
+    @callback(
+        Output("db-table", "children"),
+        Input("db-names-selector-dropdown", "value"),
+    )
+    def update_database(value):
+        return make_table(value)
 
+    def make_table(db_name):
+        database = client[db_name]
+        collection_names = database.list_collection_names()
+        values = []
+        table_layout = []
+        for collection in collection_names:
+            values = list(database[collection].find({}, {"_id": False}))
+            table_layout.append(html.H2(children=collection))
+            table_layout.append(dash_table.DataTable(values))
+        return table_layout
 
-app.layout.append(html.Div(children="", id="db-table"))
-
-
-@callback(
-    Output("db-table", "children"),
-    Input("db-names-selector-dropdown", "value"),
-)
-def update_database(value):
-    return make_table(value)
-
-
-def make_table(db_name):
-    database = client[db_name]
-    collection_names = database.list_collection_names()
-    values = []
-    table_layout = []
-    for collection in collection_names:
-        values = list(database[collection].find({}, {"_id": False}))
-        table_layout.append(html.H2(children=collection))
-        table_layout.append(dash_table.DataTable(values))
-    return table_layout
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
