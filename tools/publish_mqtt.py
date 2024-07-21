1d6aa5767c74176b6705f02a75fadec5f0f1ae98
phofmeier <phofmeier@googlemail.com>
2024-07-21 10:20:04 +0000

    clean up publish tool

--- tools/publish_mqtt.py
+++ tools/publish_mqtt.py
@@ -1,47 +1,28 @@
 import datetime
 import json
+from time import sleep
 
 import paho.mqtt.client as mqtt
 
 broker = "localhost"
 port = 1883
+published_messages = []
+
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqtt_client.connect(broker, port, 60)
 
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, reason_code, properties):
-    print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    # client.subscribe("$SYS/#")
-
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
-
-
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-
-mqttc.connect(broker, port, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-test_item = {
-    "id": "TestUniqueid2",
+test_item_static = {
+    "id": "TestUniqueid3",
     "item_name": "test_3",
     "item_val": 10.1,
     "another_thing": "as String",
 }
 
-test_item_json = json.dumps(test_item)
-
-
-mqttc.publish("data/test_db/static/node", test_item_json)
-mqttc.publish("test_2/test_node_1", "testMessage")
+published_messages.append(
+    mqtt_client.publish("data/test_db/static/node", json.dumps(test_item_static))
+)
+published_messages.append(mqtt_client.publish("test_2/test_node_1", "testMessage"))
 
 timed_item = {
     "timestamp": datetime.datetime.now().isoformat(),
@@ -49,8 +30,15 @@ timed_item = {
     "humidity": 35.4,
 }
 
-timed_item_json = json.dumps(timed_item)
 
-mqttc.publish("data/test_db/timed/temperature", timed_item_json)
+published_messages.append(
+    mqtt_client.publish("data/test_db/timed/temperature", json.dumps(timed_item))
+)
 
-mqttc.loop_forever()
+mqtt_client.loop_start()
+while mqtt_client.want_write() or any(
+    [not msg.is_published() for msg in published_messages]
+):
+    sleep(1)
+mqtt_client.loop_stop()
+mqtt_client.disconnect()
